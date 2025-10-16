1ae09660519f44786fc8af0570f7bdf092ce8061
irwingileck <120511688+irwingileck@users.noreply.github.com>
2025-10-16 00:24:49 +0000

    Atualização da pagina inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
 function Home() {
-    return <h1>Lindeza, vc é uma mulher incrivel, te amo de montão, feliz 3 anos.</h1>
+  const estiloContainer = {
+    backgroundColor: "#A0B0C0",  // por exemplo: um cinza azulado
+    minHeight: "100vh",          // para ocupar toda a altura da tela
+    margin: 0,
+    padding: 0,
+  };
+
+  return (
+    <div style={estiloContainer}>
+      <h1>Lindeza, vc é uma mulher incrivel, te amo de montão, feliz 3 anos.</h1>
+    </div>
+  );
 }
+
 
 export default Home;
