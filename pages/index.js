1974cd13c6b4355da1f774a458ea9b3bfde349ce
irwingileck <120511688+irwingileck@users.noreply.github.com>
2025-11-03 22:58:39 +0000

    Adicionado scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
 function Home() {
   const estiloContainer = {
-    backgroundColor: "#A0B0C0",  // por exemplo: um cinza azulado
-    minHeight: "100vh",          // para ocupar toda a altura da tela
+    backgroundColor: "#A0B0C0", // por exemplo: um cinza azulado
+    minHeight: "100vh", // para ocupar toda a altura da tela
     margin: 0,
     padding: 0,
   };
 
   return (
     <div style={estiloContainer}>
-      <h1>Lindeza, vc é uma mulher incrivel, te amo de montão, feliz 3 anos.</h1>
+      <h1>
+        Lindeza, vc é uma mulher incrivel, te amo de montão, feliz 3 anos.
+      </h1>
     </div>
   );
 }
-
 
 export default Home;
